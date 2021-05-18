c27f720126c3aeed5312bcf398ea27641f14fc7f
MasseRaphael <masseraphael76@gmail.com>
2021-05-18 09:56:02 +0200

    initialisation du projet

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Phaser from './lib/phaser.js';
+
+import Game from './scenes/Game.js';
+
+
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: [Game],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y:200
+            },
+            debug: true
+        }
+    }
+})
+
+console.dir(Phaser);
